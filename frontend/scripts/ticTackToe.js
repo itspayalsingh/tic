2fabbd4a106ef0c059cc3bcee89be4c8a1bdb8f7
itspayalsingh <ps56550856@gmail.com>
2023-09-21 18:18:51 +0530

    s

--- frontend/scripts/ticTackToe.js
+++ frontend/scripts/ticTackToe.js
@@ -1,4 +1,4 @@
-let socket = io("https://tic-fglh.onrender.com/", { transports: ["websocket"] })
+let socket = io("http://localhost:3000/", { transports: ["websocket"] })
 
 const myPeer = new Peer()
 let url = window.location.href
